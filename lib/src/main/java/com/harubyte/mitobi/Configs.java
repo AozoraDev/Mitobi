62b11e18b6580de24b81da4c95360c4efc4aa4ae
AozoraDev <niicoremusic@gmail.com>
2023-11-19 23:27:18 +0800

    Update to v1.2

--- lib/src/main/java/com/harubyte/mitobi/Configs.java
+++ lib/src/main/java/com/harubyte/mitobi/Configs.java
@@ -11,7 +11,7 @@ import org.json.JSONObject;
 
 public class Configs {
     public static final String NAME = "Mitobi";
-    public static final String VERSION = "1.1";
+    public static final String VERSION = "1.2";
     public static final String TITLE = String.format("%s (v%s)", NAME, VERSION);
     public static final String ACTION = "com.harubyte.mitobi.action.MAIN";
     
